1c9eeeda1357c156bc098137141be1551e0da9e7
Gleb Kletskov <gfgfddgleb@gmail.com>
2021-07-09 13:20:07 +0300

    feature/gymrecord-1: init client project

---
+++ client/src/store/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+export type RootState = {};
+
+const rootReducer = combineReducers({});
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
+store.subscribe(() => console.log(store.getState()));
