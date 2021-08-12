a5a0395b96aef425bd107e81070a452747475710
mikel <mihail.shch@gmail.com>
2021-08-12 14:53:21 +0300

    deleme mongo

--- API/libs/GymRecords.DataLayer/Abstract/IMongoDbContext.cs
+++
@@ -1,9 +0,0 @@
-using MongoDB.Driver;
-
-namespace GymRecords.DataLayer.Abstract
-{
-    public interface IMongoDbContext
-    {
-        IMongoCollection<TModel> GetCollection<TModel>(string dbName, string collectionName);
-    }
-}
